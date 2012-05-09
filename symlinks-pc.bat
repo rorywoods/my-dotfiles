0f0fb853faf1b358f5d2ef694559dac70b306d02
rory woods <rory.woods@gmail.com>
2012-05-09 11:44:29 -0400

    Add _vimrc

--- symlinks-pc.bat
+++ symlinks-pc.bat
@@ -1,2 +1,3 @@
 mklink /h %userprofile%\.gitconfig .gitconfig
+mklink /h %userprofile%\_vimrc _vimrc
 mklink /h %userprofile%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 Microsoft.PowerShell_profile.ps1
