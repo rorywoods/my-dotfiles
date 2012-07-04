cd0bbc012d318067d0b08ffd6ea101e806d1b5bb
rory woods <rory.woods@gmail.com>
2012-07-04 17:46:49 -0400

    Clean up ps profile/console2 hotkeys

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,17 +10,20 @@ Import-Module PsGet
 # Load posh-git example profile
 . 'C:\Users\Rory\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
+# Load posh-svn example profile
+#. 'C:\Users\Rory\Documents\WindowsPowerShell\Modules\posh-svn\profile.example.ps1'
+
 #alias
 set-alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
 
 #funcs
-function New-PSSecureRemoteSession
-{
-  params($sshServerName, $Cred)
-  $Session = New-PSSession $sshServerName -UseSSL - Credential $Cred -ConfigurationName C2Remote
-  Enter-PSSession -Session $Session
-}
-set-alias ssh New-PSSecureRemoteSession 
+#function New-PSSecureRemoteSession
+#{
+#  params($sshServerName, $Cred)
+#  $Session = New-PSSession $sshServerName -UseSSL - Credential $Cred -ConfigurationName C2Remote
+#  Enter-PSSession -Session $Session
+#}
+#set-alias ssh New-PSSecureRemoteSession 
 
 #posh-git
 $GitPromptSettings.WorkingForegroundColor = "Red"
